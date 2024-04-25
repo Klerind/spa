8836b7541d2ee72ae61fee7bf095d8b90c0f9289
klerind <klerindtervoli@gmail.com>
2024-04-25 13:39:54 +0000

    test

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/h', function () {
     return Inertia::render('Home/Index');
 })->middleware('guest')->name('home');
 
