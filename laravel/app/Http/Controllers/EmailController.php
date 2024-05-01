fa626b9da19c1fc4f99f0f2340433ce8e6db33fa
Klerind <klerindtervoli@gmail.com>
2024-05-01 04:55:56 +0200

    adding mail3

--- laravel/app/Http/Controllers/EmailController.php
+++ laravel/app/Http/Controllers/EmailController.php
@@ -12,7 +12,7 @@ class EmailController extends Controller
   public function sendMail(Request $request): Response
   { //dd($request);
 
-      Mail::to($request->user())->send(new SendMail($request));
+      Mail::to('test@test.com')->send(new SendMail($request));
       return response('Hello World', 200)
                   ->header('Content-Type', 'text/plain');;
   }
